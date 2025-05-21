2980a63fab21f512b2ebc4e953365f0f0360e19b
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-05-21 17:14:57 -0300

    use route defined layout

--- src/app.vue
+++ src/app.vue
@@ -1,52 +1,20 @@
 <template>
-  <RouterLayout v-if="layout">
+  <RouterLayout v-if="hasLayout">
     <RouterView />
   </RouterLayout>
   <RouterView v-else />
-
   <DevtoolsVueQuery />
 </template>
 
 <script setup>
-import { computed, defineAsyncComponent } from 'vue'
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 import DevtoolsVueQuery from '~/components/devtools/vue-query'
-import { isComp, isFn, isNil } from '~/utils/is'
-
-/**
- * @param {Omit<import("vue").AsyncComponentOptions, "loader" | "suspensible">} options
- */
-function safeExtractComponent(component, options = {}) {
-  switch (true) {
-    case isNil(component):
-      return undefined
-
-    case isComp(component):
-      return component
-
-    case isFn(component):
-      return defineAsyncComponent({
-        ...options,
-        suspensible: false,
-        loader: component,
-      })
-
-    default:
-      return undefined
-  }
-}
+import { useRouteDefinedLayout } from '~/composables/use-route-defined-layout'
 
 const route = useRoute()
+const hasLayout = computed(() => Boolean(route.meta.layout))
 
-const layout = computed(() => route.meta.layout)
-const layoutLoading = computed(() => route.meta.layoutLoading)
-const layoutError = computed(() => route.meta.layoutError)
-
-const RouterLayout = computed(() =>
-  safeExtractComponent(layout.value, {
-    loadingComponent: safeExtractComponent(layoutLoading.value),
-    errorComponent: safeExtractComponent(layoutError.value),
-  }),
-)
+const RouterLayout = useRouteDefinedLayout()
 </script>
---
+++ src/composables/use-route-defined-layout.js
@@ -0,0 +1,40 @@
+import { computed, defineAsyncComponent } from 'vue'
+import { useRoute } from 'vue-router'
+
+import { isComp, isFn, isNil } from '~/utils/is'
+
+export function useRouteDefinedLayout() {
+  const route = useRoute()
+
+  const RouterLayout = computed(() =>
+    safeExtractComponent(route.meta.layout, {
+      loadingComponent: safeExtractComponent(route.meta.layoutLoading),
+      errorComponent: safeExtractComponent(route.meta.layoutError),
+    }),
+  )
+
+  return RouterLayout
+}
+
+/**
+ * @param {Omit<import("vue").AsyncComponentOptions, "loader" | "suspensible">} options
+ */
+function safeExtractComponent(component, options = {}) {
+  switch (true) {
+    case isNil(component):
+      return undefined
+
+    case isComp(component):
+      return component
+
+    case isFn(component):
+      return defineAsyncComponent({
+        ...options,
+        suspensible: false,
+        loader: component,
+      })
+
+    default:
+      return undefined
+  }
+}
