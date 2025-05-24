e9961ac091526956cd6094ad16fa9371cd56b43a
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-05-23 22:25:52 -0300

    separa rotas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
 import { HomePageName } from '~/constants/routes'
 
 /** @type {import("vue-router").RouteRecordRaw[]} */
-const routes = [
+export const routes = [
   {
     path: '/',
     name: HomePageName,
@@ -11,8 +9,3 @@ const routes = [
     meta: { layout: () => import('~/layouts/default-layout') },
   },
 ]
-
-export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routes,
-})
